c348a779ec92f55ee5806f2cd0b603a192481081
Davi de Sá Luquini <126691508+DaviLuquini@users.noreply.github.com>
2024-10-30 17:20:40 -0300

    Delete GestaoDeProdutos directory

--- GestaoDeProdutos/src/GestaoDeProdutos/Entidades/IProdutoFabrica.java
+++
@@ -1,8 +0,0 @@
-package GestaoDeProdutos.Entidades;
-
-public interface IProdutoFabrica {
-    Produto criarProduto(int codigo, String nome, int quantidade, double preco, String manga, String tamanho);
-    
-    Produto criarProduto(int codigo, String nome, int quantidade, double preco, String cor, int comprimento);
-}
-
